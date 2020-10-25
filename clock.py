8f93324ee1bd48752509689681ad776fc9e60aaa
sumin <tn841@naver.com>
2020-10-25 21:51:31 +0900

    requests

--- clock.py
+++ clock.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=15)
 def timed_job():
     print('This job is run every 100 minutes.')
-    res = request.get('https://bk000-wedding.herokuapp.com/')
+    res = requests.get('https://bk000-wedding.herokuapp.com/')
     print(res.text)
 
 sched.start()
